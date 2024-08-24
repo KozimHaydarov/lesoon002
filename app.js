7bf4d52c47f7384f6c1f1a440218206e0aa5b13d
KozimHaydarov <kozimxaydarov208@gmail.com>
2024-08-25 01:32:21 +0300

    lesoo1

---
+++ app.js
@@ -0,0 +1,67 @@
+
+{
+    // 1-masala
+    // let a = +prompt("a")
+    // let b = +prompt("4")
+    // console.log( (4 * a) )
+}
+{
+    // 2 -masala
+    // let a = +prompt ("a")
+    // let b = +prompt ("b")
+    // console.log( ("a + b")*2)
+}
+ {   // 3 masala
+        // let c = +prompt("A soni kiriting ")
+        // let s = +prompt("B sonini  kiriting")
+        // let cCalck = s * s
+        // let sCalc = 2 * (c + s)
+        // console.log(` ${cCalck} ${sCalc}`)
+ }
+      {
+        // // 4 masala 
+        // let d = +prompt('aylanani aniqlovchi')
+        // let a = 3.14
+        // let dCALC = d * a 
+        // console.log(`aylana uzunligi ${dCALC}`);
+      }
+
+      {
+        // 5 masala
+        let e = +prompt('A ning xajmini aniqlovchi dastur')
+        let eCALC = e * e * e
+        let sCALC = 6 * e * e
+        console.log(` ${eCALC} va sirti ${sCALC} ga teng `);
+      }
+
+      {
+        // 7 masala
+        // let g = +prompt('Doiraning uzunligi aniqlovchi dastur')
+        // let l = 2 * 3.14 * g
+        // let s = 3.14 * g * g 
+        // console.log(`Doira Uzunligi anilovchi ${l}ga teng yuzasi ${s} ga teng`);
+      }
+
+      {
+        // 8 masala
+        // let a = +prompt(' A-ni kiriting')
+        // let b = +prompt(' B-ni kiriting')
+        // let abCALC = ( a + b ) / 2 
+        // console.log(`Siz kiritgan sonning ${abCALC} ga teng`);
+    }
+      {
+        // 9 masala 
+        // let a = +prompt("A sonini kiriting")
+        // let b = +prompt("B sonini kiritng ")
+        // let s = a + b
+        // let d = a * b
+        // let e = a * a
+        // let f = b * b
+        // console.log(${s} ga teng");
+    }
+
+
+
+
+
+
